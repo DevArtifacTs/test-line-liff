28dfa67ab815df1a3c817dad1bc603199c2f7a11
RaThomBoi <jessada.srm@gmail.com>
2022-09-28 16:57:54 +0700

    docs: edit liff id

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
 	const initLine = () => {
 		liff.init(
-			{ liffId: "1655665373-YAopzeO6" },
+			{ liffId: "1656508316-k7jNojXm" },
 			() => {
 				if (liff.isLoggedIn()) {
 					runApp();
